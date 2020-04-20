6ee5898c42fbb5e84800cc94478d6d6a17d474f8
madifz <m.d.faizsal@gmail.com>
2020-04-20 21:32:16 +0800

    Fixed errors with button module.

--- game_stats.py
+++ game_stats.py
@@ -5,8 +5,8 @@ class GameStats:
 		"""Initialize statistics."""
 		self.settings = ai_game.settings
 		self.reset_stats()
-		# Start ALien Invasion in an active state.
-		self.game_active = True
+		# Start ALien Invasion in an inactive state.
+		self.game_active = False
 
 	def reset_stats(self):
 		"""Initialize statistics that can change during the game."""
