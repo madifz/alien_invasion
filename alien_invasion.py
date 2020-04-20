57cec4d7c79607b4d557e9ea1af68bc1b6ad1e04
madifz <m.d.faizsal@gmail.com>
2020-04-21 00:01:11 +0800

    Updated source code to include scores, high scores, level, and ships left.

--- alien_invasion.py
+++ alien_invasion.py
@@ -85,6 +85,9 @@ class AlienInvasion:
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
+            self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -126,6 +129,9 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """
@@ -202,8 +208,9 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left.
+            # Decrement ships_left, and update the scoreboard.
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,14 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 class Scoreboard:
 	"""A class to report scoring information."""
 
 	def __init__(self, ai_game):
 		"""Initialize scorekeeping attributes."""
+		self.ai_game = ai_game
 		self.screen = ai_game.screen
 		self.screen_rect = self.screen.get_rect()
 		self.settings = ai_game.settings
@@ -16,6 +20,8 @@ class Scoreboard:
 		# Prepare the initial score images.
 		self.prep_score()
 		self.prep_high_score()
+		self.prep_level()
+		self.prep_ships()
 
 	def prep_score(self):
 		"""Turn the score into a rendered image."""
@@ -47,7 +53,29 @@ class Scoreboard:
 			self.stats.high_score = self.stats.score
 			self.prep_high_score()
 
+	def prep_level(self):
+		"""Turn the level into a rendered image."""
+		level_str = str(self.stats.level)
+		self.level_image = self.font.render(level_str, True,
+			self.text_color, self.settings.bg_color)
+
+		# Position the level below the score.
+		self.level_rect = self.level_image.get_rect()
+		self.level_rect.right = self.score_rect.right
+		self.level_rect.top = self.high_score_rect.bottom + 10
+
+	def prep_ships(self):
+		"""Show how many ships are left."""
+		self.ships = Group()
+		for ship_number in range(self.stats.ships_left):
+			ship = Ship(self.ai_game)
+			ship.rect.x = 10 + ship_number * ship.rect.width
+			ship.rect.y = 10
+			self.ships.add(ship)
+
 	def show_score(self):
-		"""Draw score to the screen."""
+		"""Draw scores and level and ships to the screen."""
 		self.screen.blit(self.score_image, self.score_rect)
 		self.screen.blit(self.high_score_image, self.high_score_rect)
+		self.screen.blit(self.level_image, self.level_rect)
+		self.ships.draw(self.screen)
